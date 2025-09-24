c50b3503d77b1470a7ad0773bf13886c9b95cf67
Viki <hi@viki.moe>
2025-09-24 09:45:21 +0800

    feat: 支持 `/list` 获取文件列表

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,24 @@
 import v50 from '../static/v50.json' with { type: 'json' }
 
 const options = { port: 8080, hostname: 'localhost' }
-Deno.serve(options, () => new Response(v50.at(Math.floor(Math.random() * v50.length))))
+
+Deno.serve(options, response => {
+  const u = new URL(response.url)
+
+  console.log(u.pathname)
+
+  switch (u.pathname) {
+    case '/favicon.ico': {
+      return Response.redirect('https://avatar.viki.moe', 302)
+    }
+
+    case '/list': {
+      return Response.json(v50)
+    }
+
+    case 'random':
+    default: {
+      return new Response(v50.at(Math.floor(Math.random() * v50.length)))
+    }
+  }
+})
